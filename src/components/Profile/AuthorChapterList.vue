71267fa80a9c5dcf1d3816db593ab223d870d048
coder319 <921506402@qq.com>
2020-11-10 15:50:36 +0800

    新增一个作者小说的章节列表目录组件

---
+++ src/components/Profile/AuthorChapterList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    11111
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "AuthorChapterList",
+    data () {
+      return {
+        // 当前折叠板的章节列表
+        list:[]
+      }
+    },
+    props:{
+      // 当前折叠板的小说ID
+      bookId:-1
+    },
+    // created() {
+    //   if(this.bookId == -1) {
+    //     alert('小说id错误！')
+    //   }
+    //   else{
+    //     this.list = this.$http.getChapterById(this.bookId)
+    //     console.log(this.list)
+    //   }
+    // }
+  }
+</script>
+
+<style scoped>
+
+</style>
