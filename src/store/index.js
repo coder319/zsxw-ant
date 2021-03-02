62f60f19a45738435e521b3b733ba2e9732fd78a
wangtr <921506402@qq.com>
2021-03-02 16:56:15 +0800

    修改接口地址

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Wangtr
  * @Date: 2020-08-16 23:47:53
- * @LastEditTime: 2021-02-01 20:41:47
+ * @LastEditTime: 2021-03-02 16:55:28
  * @LastEditors: Wangtr
  * @Reference: 
  */
@@ -14,7 +14,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     // apiHost:'http://192.168.43.100',
-    apiHost: 'http://118.31.229.85',
+    // apiHost: 'http://118.31.229.85',
+    apiHost: 'http://49.235.226.27',
     // apiHost: 'http://127.0.0.1',
     apiPost: ':9999',
     userState: -1,
